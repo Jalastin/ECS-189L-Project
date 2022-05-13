7fde65731b9ee1e65006a88fb1d911970914dde9
Jalastin <justin_rusit@yahoo.com>
2022-05-12 18:20:09 -0700

    fixed basic scene; added sorting layers

--- ECS 189L Game/Assets/Scripts/ProjectileController.cs
+++ ECS 189L Game/Assets/Scripts/ProjectileController.cs
@@ -8,7 +8,7 @@ public class ProjectileController : MonoBehaviour
 
     void Start()
     {
-        this.transform.localScale = new Vector3(0.01f, 0.01f, 0.01f);
+        
     }
 
     void Update()
