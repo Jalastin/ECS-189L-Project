b3b30e3175bab7f06ede5539c4370a9b747851fc
Jalastin <justin_rusit@yahoo.com>
2022-05-15 16:56:32 -0700

    tried adding platform collision

--- ECS 189L Game/Assets/Scripts/ProjectileController.cs
+++ ECS 189L Game/Assets/Scripts/ProjectileController.cs
@@ -8,8 +8,16 @@ public class ProjectileController : MonoBehaviour
 
     private void OnTriggerEnter(Collider other)
     {
+        Debug.Log("Contact!");
         Destroy(this.gameObject);
     }
+
+    // private void OnCollisionEnter2D(Collision2D other)
+    // {
+    //     Debug.Log("Contact!");
+    //     Destroy(this.gameObject);
+    // }
+
 
     void Start()
     {
