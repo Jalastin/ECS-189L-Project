2569b64cbbaba65b0bea4143320f7ab339334f32
Jalastin <justin_rusit@yahoo.com>
2022-05-15 19:21:12 -0700

    fixed collision w/ platforms!

--- ECS 189L Game/Assets/Scripts/ProjectileController.cs
+++ ECS 189L Game/Assets/Scripts/ProjectileController.cs
@@ -6,7 +6,7 @@ using UnityEngine;
 public class ProjectileController : MonoBehaviour
 {
 
-    private void OnTriggerEnter(Collider other)
+    private void OnTriggerEnter2D(Collider2D other)
     {
         Debug.Log("Contact!");
         Destroy(this.gameObject);
