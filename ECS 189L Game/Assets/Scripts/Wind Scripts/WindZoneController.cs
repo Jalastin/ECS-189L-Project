ef55db4d0223d7016694831274835176ef78c310
Jalastin <justin_rusit@yahoo.com>
2022-05-17 21:35:02 -0700

    added random spawning of more WindSprites

--- ECS 189L Game/Assets/Scripts/Wind Scripts/WindZoneController.cs
+++ ECS 189L Game/Assets/Scripts/Wind Scripts/WindZoneController.cs
@@ -11,6 +11,19 @@ public class WindZoneController : MonoBehaviour
         get => _windForce;
         set => _windForce = value;
     }
+
+    // Minimum time a WindSprite can spawn after the last WindSprite.
+    [SerializeField] private float minWindSpriteSpawnTime = 1f;
+
+    // Maximum time a WindSprite can spawn after the last WindSprite.
+    [SerializeField] private float maxWindSpriteSpawnTime = 5f;
+
+    // Time for the next WindSprite to spawn.
+    private float spriteSpawnTime;
+
+    // timeElapsed since last WindSprite spawn.
+    private float timeElapsed;
+
     void OnTriggerStay2D(Collider2D other)
     {
         // Do not affect platforms.
@@ -20,7 +33,21 @@ public class WindZoneController : MonoBehaviour
             other.GetComponent<Rigidbody2D>().AddForce(vector);
         }
     }
-    void Start() {
+    void Start()
+    {
         this.GetComponent<WindSpriteFactory>().GenerateRandomWindSprite();
+        this.spriteSpawnTime = Random.Range(this.minWindSpriteSpawnTime, this.maxWindSpriteSpawnTime);
+        this.timeElapsed = 0f;
+    }
+
+    void Update()
+    {
+        if (this.timeElapsed >= this.spriteSpawnTime)
+        {
+            this.GetComponent<WindSpriteFactory>().GenerateRandomWindSprite();
+            this.spriteSpawnTime = Random.Range(1f, 5f);
+            this.timeElapsed = 0f;
+        }
+        this.timeElapsed += Time.deltaTime;
     }
 }
