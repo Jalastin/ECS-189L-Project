e64b2b58269cb8d6d9246a4e5a426c62aaff6ebe
Devin <ly.devin.177@gmail.com>
2022-05-24 18:58:40 -0700

    First stage of city started

--- ECS 189L Game/Assets/Flat Platformer Template/Scripts/WindMill.cs
+++ ECS 189L Game/Assets/Flat Platformer Template/Scripts/WindMill.cs
@@ -4,15 +4,9 @@ using UnityEngine;
 
 public class WindMill : MonoBehaviour {
     public float _Speed;
-    private float _currDist, _dist;
 
     void Update()
     {
-        if(Mathf.Round(_currDist) == Mathf.Round(_dist) || _currDist == 0)
-        {
-            _dist = Random.Range(0, 2.7f);
-        }
-        _currDist = Mathf.Lerp(_currDist, _dist, Time.deltaTime*0.3f);
-        transform.Rotate(0, 0, _Speed * Time.deltaTime + _currDist);
+        transform.Rotate(0, 0, _Speed * Time.deltaTime);
     }
 }
--- ECS 189L Game/Assets/Scripts/CameraController.cs
+++ ECS 189L Game/Assets/Scripts/CameraController.cs
@@ -10,6 +10,15 @@ public class CameraController : MonoBehaviour
         get => _mousePosition;
         set => _mousePosition = value;
     }
+    private Camera managedCamera;
+    [SerializeField] private GameObject target;
+    [SerializeField] private float Speed;
+    // Initialize the camera and line-renderer.
+    private void Awake()
+    {
+        this.managedCamera = this.gameObject.GetComponent<Camera>();
+    }
+
     // Start is called before the first frame update
     void Start()
     {
@@ -19,7 +28,22 @@ public class CameraController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        // The camera is set to the target's position (except the z-value).
+        var targetPosition = this.target.transform.position;
+        var cameraPosition = this.managedCamera.transform.position;
+        Vector3 cameraTarget = new Vector3(targetPosition.x, targetPosition.y+10, cameraPosition.z);
+        // Only move the camera if it is not already on the target.
+        if (cameraPosition != cameraTarget)
+        {
+            // Target is moving, but within leash; move camera at fractional target's speed.
+            cameraPosition = Vector3.MoveTowards(cameraPosition,
+                cameraTarget, Time.deltaTime * this.Speed);
+        }
+
+        this.managedCamera.transform.position = cameraPosition;
+
         // Update MousePosition to wherever the mouse currently is.
         MousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
     }
 }
+
