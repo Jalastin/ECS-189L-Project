5ba9a23bcd68643f5f70de2156b1479a7f999b7b
Alec Atienza <alec@null.net>
2022-05-25 23:40:28 -0700

    added base game states

--- ECS 189L Game/Assets/Scripts/GameManager.cs
+++ ECS 189L Game/Assets/Scripts/GameManager.cs
@@ -1,20 +1,43 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+// Lots of inspiration taken from https://www.youtube.com/watch?v=4I0vonyqMi8.
 public class GameManager : MonoBehaviour
 {
-    
+    private static GameManager instance;
+    public static GameManager Instance 
+    { 
+        get => instance;
+    }
+    private GameState currentState;
+    public GameState CurrentState 
+    { 
+        get => currentState;
+    }
+    public static event Action<GameState> OnGameStateChanged;
 
-    // Start is called before the first frame update
-    void Start()
+    void Awake()
     {
-        
+        instance = this;
     }
 
-    // Update is called once per frame
-    void Update()
+    public void UpdateGameState(GameState newState)
     {
-        
+        this.currentState = newState;
+
+        switch (newState)
+        {
+            case GameState.MainMenu:
+                break;
+            case GameState.Paused:
+                break;
+            case GameState.Playing:
+                break;
+        }
+
+        // Run respective callbacks of subscribed components.
+        OnGameStateChanged?.Invoke(newState);
     }
 }
