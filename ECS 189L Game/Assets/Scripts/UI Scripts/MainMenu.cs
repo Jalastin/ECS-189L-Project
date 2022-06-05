9fcb7a039bfc027ace2402f32f8b9c51fc0ea359
Cameron Yee <yeec327@gmail.com>
2022-06-05 11:23:43 -0700

    Only displayed console cursor when console controller is connected

--- ECS 189L Game/Assets/Scripts/UI Scripts/MainMenu.cs
+++ ECS 189L Game/Assets/Scripts/UI Scripts/MainMenu.cs
@@ -2,12 +2,32 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+using UnityEngine.InputSystem;
+using UnityEngine.InputSystem.LowLevel;
+using UnityEngine.InputSystem.Users;
+
 public class MainMenu : MonoBehaviour
 {
+    private Color curColor;
+
+    void Awake()
+    {
+        this.curColor = GameObject.Find("Cursor").GetComponent<Image>().color;
+    }
+    
     // Background music should be playing by default
     void Start()
     {
         AudioListener.pause = false;
+        if (Gamepad.current == null)
+        {
+            GameObject.Find("Cursor").GetComponent<Image>().color = new Color(this.curColor.r, this.curColor.g, this.curColor.b, 0f);
+        }
+        else
+        {
+            GameObject.Find("Cursor").GetComponent<Image>().color = new Color(this.curColor.r, this.curColor.g, this.curColor.b, 1f);
+        }
     }
 
     public void ToggleStart()
